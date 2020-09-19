ca3b4bf31f7f7ae2ebef306fadf7eb7d328f9876
Do Le Duy <>
2020-09-19 14:05:26 +0700

    Update

---
+++ QuickChat/Presenter/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  QuickChat
+//
+//  Created by Do Le Duy on 9/19/20.
+//  Copyright © 2020 Haik Aslanyan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+  
+  //MARK: Life cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .darkGray
+  }
+}
